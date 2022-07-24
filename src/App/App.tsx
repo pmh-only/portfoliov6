9f41bee7803ff0a844e6ab1dce3e92cd6158e7c8
Park Min Hyeok <opensource@pmh.codes>
2022-07-24 17:11:51 +0900

    Revert "feat: add page animation"
    This reverts commit 7e863038bc8e15bdd8dac68339cb28a7be36c926.

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,6 +1,5 @@
 import { Fragment } from 'react'
 import HelloJumbotron from '../HelloJumbotron/HelloJumbotron'
-import Keywords from '../Keywords/Keywords'
 
 import './App.module.scss'
 
@@ -14,7 +13,6 @@ const App = () =>
 
     <main>
       <HelloJumbotron />
-      <Keywords />
     </main>
 
     <footer></footer>
--- src/HelloJumbotron/HelloJumbotron.tsx
+++ src/HelloJumbotron/HelloJumbotron.tsx
@@ -1,34 +1,11 @@
 import style from './HelloJumbotron.module.scss'
-import usePage from 'use-motion-page'
-import { motion, useTransform } from 'framer-motion'
-import { useEffect, useState } from 'react'
 
-const HelloJumbotron = () => {
-  const page = usePage()
-  const padding = useTransform(page, [0, 0.3], [0, 20])
-  const opacity = useTransform(page, [0, 0.45], [1, 0])
+const HelloJumbotron = () =>
+  <article id="hello" className={style.container}>
+    <h1 className={style.title}>
+      안녕하세요, <strong>박민혁</strong>입니다.
+    </h1>
+    <p><code>#next</code>까지 스크롤을 내려주세요</p>
+  </article>
 
-  const [isVisible, setVisible] = useState(true)
-
-  useEffect(() => {
-    page.onChange((v) => {
-      setVisible(v < 0.5)
-    })
-  }, [])
-
-  return (
-    <motion.article id="hello" style={{ padding, opacity }}>
-      <motion.div className={style.container}>
-        {isVisible && (
-          <div className={style.hello}>
-            <h1 className={style.title}>
-              안녕하세요, <strong>박민혁</strong>입니다.
-            </h1>
-            <p><a href="#keywords"><code>#keywords</code></a>까지 스크롤을 내려주세요</p>
-          </div>
-        )}
-      </motion.div>
-    </motion.article>
-  )
-}
 export default HelloJumbotron
