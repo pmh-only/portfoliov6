fd9a7ea03b6abab9ad392946caa4fab0e25fcd0f
Park Min Hyeok <opensource@pmh.codes>
2022-07-24 17:17:43 +0900

    feat: add typing animation

--- src/HelloJumbotron/HelloJumbotron.tsx
+++ src/HelloJumbotron/HelloJumbotron.tsx
@@ -1,9 +1,15 @@
+import TypeAnimation from 'react-type-animation'
 import style from './HelloJumbotron.module.scss'
 
 const HelloJumbotron = () =>
   <article id="hello" className={style.container}>
     <h1 className={style.title}>
-      안녕하세요, <strong>박민혁</strong>입니다.
+      <TypeAnimation
+        cursor={false}
+        sequence={[
+          1000, '안녕하세요,',
+          1000, '안녕하세요, 박민혁입니다.'
+        ]} />
     </h1>
     <p><code>#next</code>까지 스크롤을 내려주세요</p>
   </article>
